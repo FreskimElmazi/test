01c4214dac576b6f74fdfcd29a7d86558604a14b
FreskimElmazi <34135369+FreskimElmazi@users.noreply.github.com>
2018-07-11 14:05:30 +0200

    First commit
    First commit files

---
+++ src/js/script.js
@@ -0,0 +1,82 @@
+//Name Function
+
+var myName = "Ensar";
+var myLastName = "Shasivari";
+var myFullName = myName + " " + myLastName;
+
+function showMyName() {
+    alert(myFullName);
+
+}
+
+//Money Function
+
+var myText = "I have";
+var myText2 = "100.000.000 $";
+var myText3 = "in my bank account.";
+var myFullMoney = myText + " " + myText2 + " " + myText3;
+
+function showMyMoney() {
+    alert(myFullMoney);
+
+}
+
+//DateBirth Function
+
+var myBirthText1 = "My Birthdate";
+var myBirthDate = "25/08/1997";
+var myFullDate = myBirthText1 + " " + myBirthDate;
+
+function showMyBirthDate() {
+    alert(myFullDate);
+
+}
+
+//My Activity Function
+
+var myActivityText1 = "I spend my free time by ,";
+var myActivityText2 = "playing video games.";
+var myFullActivity = myActivityText1 + " " + myActivityText2;
+
+function showMyActivity() {
+    alert(myFullActivity);
+
+}
+
+var myFavouritesText1 = "Pizza ,";
+var myFavouritesText2 = "Doner ,";
+var myFavouritesText3 = "Yuvallatma ,";
+var myFavouritesText4 = "IceCream ,";
+var myFavouritesText5 = "Tiramisu.";
+var myFullFavourites = myFavouritesText1 + " " + myFavouritesText2 + " " + myFavouritesText3 + " " + myFavouritesText4 + " " + myFavouritesText5;
+
+
+function showMyFavourites() {
+    alert(myFullFavourites);
+
+}
+
+
+//My Aggressive Function
+
+var myAggressiveText1 = "I don't fcking care your opinion about me , ";
+var myAggressiveText2 = "im salty against retarded people.";
+var myFullAggressive = myAggressiveText1 + " " + myAggressiveText2;
+
+function showMyAggressive() {
+    alert(myFullAggressive);
+
+}
+
+//GetMap function
+
+
+
+
+
+
+
+
+
+
+
